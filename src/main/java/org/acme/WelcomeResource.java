ddf7811f0bf24bcec50ff1c47337f8afb63716ac
Manuel <tiempor3all@gmail.com>
2024-04-16 23:18:07 -0600

    Modified logic to handle SSE. Example from Mutiny website

--- src/main/java/org/acme/WelcomeResource.java
+++ src/main/java/org/acme/WelcomeResource.java
@@ -1,19 +1,33 @@
 package org.acme;
 
 import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Multi;
+import io.vertx.core.impl.ConcurrentHashSet;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
-
-import io.smallrye.mutiny.Multi;
-import jakarta.ws.rs.sse.Sse;
-import jakarta.ws.rs.sse.SseEventSink;
+import org.jboss.resteasy.reactive.RestStreamElementType;
 
 import java.time.Duration;
+import java.util.stream.Collectors;
+
 @Path("/")
 public class WelcomeResource {
+
+    private ConcurrentHashSet<String> identifiers = new ConcurrentHashSet<>();
+
+    private Multi<String> ticks = Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+            .onItem().transform(tick -> {
+                Log.info("tick");
+                return identifiers.stream().collect(Collectors.joining(","));
+            })
+            .onSubscription().invoke(() -> Log.info("Starting to emit ticks"))
+            .onCancellation().invoke(() -> Log.info("No more ticks"))
+            .broadcast()
+            .withCancellationAfterLastSubscriberDeparture()
+            .toAtLeast(1);
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -21,31 +35,17 @@ public class WelcomeResource {
         return "This a Quarkus application running in K3S to test CI/CD pipeline";
     }
 
-    @Path("sse")
+
+
     @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void streamEvents(@Context SseEventSink eventSink, @Context Sse sse) {
-        Multi<String> events = Multi.createFrom().ticks().every(Duration.ofSeconds(1))
-                .onItem().transform(n -> "message " + n);
-
-
-        events.subscribe().with(
-                data -> {
-                    if(!eventSink.isClosed()) {
-                        eventSink.send(sse.newEvent(data));
-                    }
-                },
-                failure -> {
-                    Log.info("failure " + failure.getMessage());
-                    if(!eventSink.isClosed()) {
-                        eventSink.close();
-                    }
-                },
-                () -> {
-
-                    Log.info("Complete");
-
-                }
-        );
+    @Path("ticks/{id}")
+    @RestStreamElementType(MediaType.TEXT_PLAIN)
+    public Multi<String> ticks(String id) {
+        Log.info("New client with id " + id);
+        identifiers.add(id);
+        return ticks.onCancellation().invoke(() -> {
+            Log.info("Removing client with id " + id);
+            identifiers.remove(id);
+        });
     }
 }
