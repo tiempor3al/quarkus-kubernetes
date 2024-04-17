fed5780346e03d482afcac8519c339658c9c9811
Manuel <tiempor3all@gmail.com>
2024-04-16 22:57:12 -0600

    Removed reactive pg extensions and added sse events endpoint

--- src/main/java/org/acme/WelcomeResource.java
+++ src/main/java/org/acme/WelcomeResource.java
@@ -1,10 +1,17 @@
 package org.acme;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 
+import io.smallrye.mutiny.Multi;
+import jakarta.ws.rs.sse.Sse;
+import jakarta.ws.rs.sse.SseEventSink;
+
+import java.time.Duration;
 @Path("/")
 public class WelcomeResource {
 
@@ -12,5 +19,33 @@ public class WelcomeResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String welcome() {
         return "This a Quarkus application running in K3S to test CI/CD pipeline";
+    }
+
+    @Path("sse")
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void streamEvents(@Context SseEventSink eventSink, @Context Sse sse) {
+        Multi<String> events = Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+                .onItem().transform(n -> "message " + n);
+
+
+        events.subscribe().with(
+                data -> {
+                    if(!eventSink.isClosed()) {
+                        eventSink.send(sse.newEvent(data));
+                    }
+                },
+                failure -> {
+                    Log.info("failure " + failure.getMessage());
+                    if(!eventSink.isClosed()) {
+                        eventSink.close();
+                    }
+                },
+                () -> {
+
+                    Log.info("Complete");
+
+                }
+        );
     }
 }
