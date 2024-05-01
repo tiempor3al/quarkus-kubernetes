56f30330b38a3ccf0150147b4abfaaf12b1a2e9e
Manuel <tiempor3all@gmail.com>
2024-05-01 09:02:14 -0600

    Added client options for heartbeat

---
+++ src/main/java/org/acme/config/AmqpConfig.java
@@ -0,0 +1,22 @@
+package org.acme.config;
+
+import io.quarkus.logging.Log;
+import io.smallrye.common.annotation.Identifier;
+import io.vertx.amqp.AmqpClientOptions;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Produces;
+
+
+@ApplicationScoped
+public class AmqpConfig {
+
+    @Produces
+    @Identifier("amqp-options")
+    public AmqpClientOptions getAmqpClientOptions() {
+
+        Log.info("Using amqp options");
+        return new AmqpClientOptions()
+                .setHeartbeat(30);
+    }
+}
+
